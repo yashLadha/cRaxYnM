b4fd56236860dc3d69040315bc280901b9a6b100
Yash Ladha <yash@yashladha.in>
2022-09-10 11:46:48 +0530

    add: theme toggle button switch
    Added theme toggle button switch which will turn to night or day based
    on the system theme or explicit option choosen from the UI.
    
    Actual implementation of theme is yet to be done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:craxynm/result.dart';
+import 'package:rive/rive.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,9 +12,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Cr@xYnM',
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: const Main(title: 'Cr@xYnM'),
     );
   }
@@ -28,23 +28,51 @@ class Main extends StatefulWidget {
   State<Main> createState() => _MainState();
 }
 
-class _MainState extends State<Main> {
+class _MainState extends State<Main> with WidgetsBindingObserver {
   String queryText = "";
   String inputText = "";
+  bool isLightMode = true;
+  SMITrigger? _toggleSwitch;
+  SMIBool? _setLightTheme;
 
   final inputTextController = TextEditingController();
 
   @override
   void initState() {
-    setState(() {
-      queryText = "";
-    });
+    WidgetsBinding.instance.addObserver(this);
     super.initState();
   }
 
   @override
-  void didUpdateWidget(Main old) {
-    super.didUpdateWidget(old);
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    decideThemeSwitch();
+    super.didChangePlatformBrightness();
+  }
+
+  bool isLightModeEnabled() {
+    return MediaQuery.of(context).platformBrightness == Brightness.light;
+  }
+
+  void decideThemeSwitch() {
+    isLightMode = isLightModeEnabled();
+    _setLightTheme?.change(isLightMode);
+    _toggleSwitch?.fire();
+  }
+
+  void _onRiveInit(Artboard artboard) {
+    final controller =
+        StateMachineController.fromArtboard(artboard, "switcher");
+    artboard.addController(controller!);
+    _toggleSwitch = controller.findInput<bool>("switch") as SMITrigger;
+    _setLightTheme = controller.findInput<bool>("light mode") as SMIBool;
+
+    decideThemeSwitch();
   }
 
   @override
@@ -60,6 +88,25 @@ class _MainState extends State<Main> {
             fontWeight: FontWeight.bold,
           ),
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 50,
+              height: 50,
+              child: GestureDetector(
+                onTap: () {
+                  _toggleSwitch?.fire();
+                },
+                child: RiveAnimation.asset(
+                  "assets/rive/switch_day_night.riv",
+                  fit: BoxFit.contain,
+                  onInit: _onRiveInit,
+                ),
+              ),
+            ),
+          ),
+        ],
         centerTitle: true,
       ),
       body: Center(
